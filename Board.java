be4bd6ed4f5893c4ec1153762374f2f62e316519
glorio <glorio_y@yahoo.com>
2013-11-06 10:48:00 -0800

    addgrid

--- Board.java
+++ Board.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Graphics2D;
 import javax.swing.*;
+import java.awt.*;
 /**
  * create a two colors of squares chessboard.
  * 
@@ -16,6 +17,8 @@ public class Board
     private int sizeOfSquare = 8;
     private int x, y;
     private JPanel[][] pnlChessCells = new JPanel[sizeOfSquare][sizeOfSquare];
+    private JPanel pnlMain = new JPanel(new GridLayout(8,8));
+
     /**
      * Constructor for objects of class Board
      */
@@ -26,13 +29,45 @@ public class Board
         
     }
 
-    public void draw (Graphics2D G, int x, int y, int sizeInPixel)
+    public void draw (Graphics2D G, int j, int k, int sizeInPixel)
     {
         G.setColor(colorOne);
         G.drawRect(x, y, sizeInPixel, sizeInPixel);
         G.fillRect(x,y,sizeInPixel,sizeInPixel);
-        this.x = x;
-        this.y = y;
+        this.x = j;
+        this.y = k;
+        for (int y = 0; y < sizeOfSquare; y++)
+
+                  for (int x = 0; x < sizeOfSquare; x++)
+
+                  {
+
+                        pnlChessCells[y][x] = new JPanel(new BorderLayout());
+
+                        pnlMain.add(pnlChessCells[y][x]);
+
+                        if (y % 2 == 0)
+
+                              if (x % 2 != 0)
+
+                                    pnlChessCells[y][x].setBackground(colorOne);
+
+                              else
+
+                                    pnlChessCells[y][x].setBackground(colorTwo);
+
+                        else
+
+                              if (x % 2 == 0)
+
+                                    pnlChessCells[y][x].setBackground(colorOne);
+
+                              else
+
+                                    pnlChessCells[y][x].setBackground(colorTwo);
+
+                  }
+
    
     }
     
